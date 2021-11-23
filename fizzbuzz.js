64b63c3fae4622f260f6acc37014ffba1f822e71
maddrake220 <jwchoi4118@gmail.com>
2021-11-23 15:30:06 +0900

    fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 Array.from(Array(100).keys()).forEach((i) => {
-  let fizz = i % 3 === 0,
-    buzz = i % 5 === 0;
-  console.log(fizz ? (buzz ? "fizzbuzz" : "fizz") : buzz ? "buzz" : i);
+  let fizz = (i + 1) % 3 == 0,
+    buzz = (i + 1) % 5 == 0;
+  console.log(fizz ? (buzz ? "fizzbuzz" : "fizz") : buzz ? "buzz" : i + 1);
 });
