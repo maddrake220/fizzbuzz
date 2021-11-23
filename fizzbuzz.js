5f141fbb1e2a51afc2bacec873a387f85c27df34
maddrake220 <jwchoi4118@gmail.com>
2021-11-23 14:52:54 +0900

    buzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,7 @@
 let answer = "";
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0) {
-    answer += "fizz\n";
+  if (i % 5 === 0) {
+    answer += "buzz\n";
   } else {
     answer += `${i}\n`;
   }
